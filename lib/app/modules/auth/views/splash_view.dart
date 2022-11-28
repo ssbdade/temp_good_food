d2131044ac4b509b389f5b3fceba69e03f7fc64c
ssb.dade <misogiacc@gmail.com>
2022-11-28 19:09:12 +0700

    cook book

--- lib/app/modules/auth/views/splash_view.dart
+++ lib/app/modules/auth/views/splash_view.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:temp_good_food/app/utils/constants/app_image.dart';
 import 'package:temp_good_food/app/modules/common/app_button.dart';
 import 'package:temp_good_food/app/utils/constants/text_const.dart';
@@ -13,59 +15,132 @@ class SplashView extends GetView<AuthController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: Get.isDarkMode
-                ? const AssetImage(AppImage.darkSplashBg)
-                : const AssetImage(AppImage.lightSplashBg),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            const Spacer(
-              flex: 5,
-            ),
-            Center(
-              child: Text(
-                KeyConst.goodFood,
-                style: TextStyle(
-                  fontSize: 36,
-                  fontWeight: FontWeight.w600,
+      body: Padding(
+        padding: EdgeInsets.only(left: width(12), top: height(30)),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerRight,
+                child: SvgPicture.asset(
+                  AppImage.threeDotIcon,
                   color: Theme.of(context).colorScheme.onBackground,
+                  height: height(24),
                 ),
               ),
-            ),
-            const Spacer(
-              flex: 1,
-            ),
-            Text(
-              KeyConst.deliciouslySimple,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-                color: Theme.of(context).colorScheme.onBackground,
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'My CookBook',
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontSize: size(28),
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
               ),
-            ),
-            const Spacer(
-              flex: 18,
-            ),
-            AppButton(
-              onTap: () {
-                controller.toAuthPage();
-              },
-              buttonTitle: KeyConst.startCooking,
-              width: 275,
-              height: height(15),
-              borderRadius: BorderRadius.circular(24),
-              buttonColor: Theme.of(context).colorScheme.primary,
-            ),
-            const Spacer(
-              flex: 3,
-            ),
-          ],
+              SizedBox(
+                height: height(550),
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 4,
+                    itemBuilder: (_, int index) {
+                      return InkWell(
+                        onTap: () {},
+                        child: SizedBox(
+                          height: height(550),
+                          width: width(250),
+                          child: Card(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(radius(18)),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    height: height(400),
+                                    decoration: BoxDecoration(
+                                        color:
+                                            Theme.of(context).colorScheme.primary,
+                                        borderRadius:
+                                            BorderRadiusDirectional.only(
+                                                topEnd:
+                                                    Radius.circular(radius(18)),
+                                                topStart:
+                                                    Radius.circular(radius(18)))),
+                                  ),
+                                  Text(
+                                    'Title $index',
+                                    style: TextStyle(
+                                      fontSize: size(24),
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                  Row(
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: [
+                                      Text('World Food'),
+                                      Spacer(),
+                                      Text('1,2K'),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      RatingBar.builder(
+                                          itemBuilder: (_,__) {
+                                            return Icon(Icons.star,
+                                              color: Colors.amber,
+                                            );
+                                          },
+                                          itemSize: 24,
+                                          initialRating: 4,
+                                          maxRating: 5,
+                                          minRating: 1,
+                                          onRatingUpdate: (rating) {
+                                            print(rating);
+                                          }),
+                                      Spacer(),
+                                      Text('Cooked'),
+                                    ],
+                                  )
+                                ],
+                              )),
+                        ),
+                      );
+                    }, separatorBuilder: (_, int index) { 
+                      return SizedBox(
+                        width: width(15),
+                      );
+                },),
+              ),
+              Text(
+                'Cooked Recipes',
+              ),
+              SizedBox(
+                height: 100,
+                child: ListView.separated(
+                  padding: EdgeInsets.zero,
+                  itemCount: 4,
+                  itemBuilder: (_, int index)
+                  => Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.red,
+                      ),
+                      Text('Cooked $index')
+                    ],
+                  ), separatorBuilder: (BuildContext context, int index) {
+                    return SizedBox(
+                      height: 12,
+                    );
+                },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
